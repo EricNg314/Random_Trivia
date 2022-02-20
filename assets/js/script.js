9457adbf0bbe6447f1af570324084640abb40389
Okens <Ng.Eric314@gmail.com>
2022-02-20 01:51:52 -0800

    Adding variable settings and api call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 console.log('test')
 
-var questions = {}
-
 var titleMessage = document.querySelector("#titleMessage");
 var startButton = document.querySelector("#start-button");
 var questionBox = document.querySelector('#question-box');
+
+var timeDefault = 60;
+var timeAmount = 0;
+var questions = {}
+var questionNumber = 0;
+var score = 0;
 
 startButton.addEventListener("click", function(event){
   event.preventDefault();
@@ -18,8 +22,13 @@ function clickStartButton() {
   clickStartGame();
 }
 
-function clickStartGame () {
-  // call getQuestions 
+async function clickStartGame () {
+  questions = await getQuestions()
+  questionNumber = 0;
+  score = 0;
+  timeAmount = timeDefault;
+  // console.log('test2')
+  console.log(questions)
   // set time interval timer of 1second.
     // increment time
     // call updateTime interval
@@ -36,8 +45,17 @@ function clickStartGame () {
     // clear time interval
 }
 
-// function getQuestions
+async function getQuestions () {
   // questionBox.textContent = ""
+  var apiResponse = await fetch('https://api.trivia.willfry.co.uk/questions?limit=20')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data)
+    return data;  
+  });
+
+  return apiResponse;  
+}
 
 
 // function update time interval
@@ -63,6 +81,9 @@ function clickStartGame () {
  // show restartButton
   // add on click call clickRestartButton
 
+// function clickSaveScore
+  // save to localstorage.
+  // display score.
 
 // function clickRestartButton
  // if call startGame, reset game/scores.
