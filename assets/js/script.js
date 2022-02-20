af7c9ff047ddb2102d291a69e6272a226a6849f3
Okens <Ng.Eric314@gmail.com>
2022-02-20 00:25:23 -0800

    Adding logic flow.

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+console.log('test')
+
+
+// function startButton
+ // hide startButton
+ // on click call startGame
+
+// function startGame
+  // set time interval timer of 1second.
+    // increment time
+     // call updateTime interval
+     // call updateQuestion
+
+     // call addAnswerOptions
+      // add on click to answers container
+       // call clickCheckAnswer
+       // call updateScore
+
+     // check time if 0 .
+      // call endGame.
+
+    // clear time interval
+
+
+
+
+
+
+// function update time interval
+// set time interval
+
+// function updateTime
+ // display new time
+
+// function updateQuestion
+
+// function addAnswerOptions
+ 
+
+// function clickCheckAnswer
+ // return true/false for answer
+
+// function updateScore
+
+
+// function endGame
+ // show saveScore button
+  // add on click call clickSaveScore button.
+ // show restartButton
+  // add on click call clickRestartButton
+
+
+// function clickRestartButton
+ // if call startGame, reset game/scores.
+
